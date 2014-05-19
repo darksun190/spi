1641608dbc0f6c69e179171503d9f65af4ccf9d8
darksun <darksun190@gmail.com>
2014-05-19 11:43:21 +0200

    fix bug for transpose

--- sp_reader/Alignment.cs
+++ sp_reader/Alignment.cs
@@ -25,7 +25,7 @@ namespace SPInterface
         );
         }
         public Alignment(XmlNode node)
-            : this ()
+            : this()
         {
             if (node.Attributes.Count == 0)
                 return;
@@ -35,9 +35,9 @@ namespace SPInterface
                 " ",
                 "|"
             };
-            double[] array = 
+            double[] array =
                 content
-                .Split(seperator,System.StringSplitOptions.RemoveEmptyEntries)
+                .Split(seperator, System.StringSplitOptions.RemoveEmptyEntries)
                 .Select(n => Convert.ToDouble(n))
                 .ToArray();
 
@@ -48,16 +48,16 @@ namespace SPInterface
             //    for (int j = 0; j < 4; ++j)
             //        array_2d[i,j] = array[index++];
 
-            trans_matrix = new DenseMatrix(4,4,array);
+            trans_matrix = new DenseMatrix(4, 4, array);
         }
         public Alignment(DenseVector vec, DenseVector pos, string name = "feature_alignment")
-            :this(vec[0], vec[1], vec[2], pos[0], pos[1], pos[2], name)
+            : this(vec[0], vec[1], vec[2], pos[0], pos[1], pos[2], name)
         {
-            
+
         }
         public Alignment(double i, double j, double k, double x = 0, double y = 0, double z = 0, string name = "feature_alignment")
         {
-            trans_matrix =new DenseMatrix(4,4,
+            trans_matrix = new DenseMatrix(4, 4,
             new double[]{
                      k / Math.Sqrt ( 1-j*j ),      0.0,                    -i / Math.Sqrt ( 1-j*j )    ,   -x, 
                      - j*i / Math.Sqrt ( 1-j*j ),  Math.Sqrt ( 1-j*j ),    - j*k / Math.Sqrt ( 1-j*j ) ,   -y, 
@@ -77,8 +77,22 @@ namespace SPInterface
         public Alignment Transpose()
         {
             DenseMatrix t_matrix = DenseMatrix.OfMatrix(trans_matrix.Transpose());
+            t_matrix[3, 0] = -t_matrix[0, 3];
+            t_matrix[3, 1] = -t_matrix[1, 3];
+            t_matrix[3, 2] = -t_matrix[2, 3];
+            t_matrix[0, 3] = 0;
+            t_matrix[1, 3] = 0;
+            t_matrix[2, 3] = 0;
+
             Alignment result = new Alignment(t_matrix);
             result.Name = this.Name;
+            return result;
+        }
+        public override string ToString()
+        {
+            string result;
+            result = Name+"\n";
+            result += trans_matrix.ToString();
             return result;
         }
     }
